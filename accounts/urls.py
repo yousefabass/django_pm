741eab018b64bfae4bbc4958d793d98f47693c0e
yousefabass <yraa912008@gmail.com>
2024-04-07 23:44:50 +0200

    Registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.views import LoginView
 from django.urls import path, include
 from accounts.forms import UserLoginForm
+from accounts.views import RegisterView  ,edit_profile
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [path('login/', LoginView.as_view(authentication_form=UserLoginForm), name='login'),
-               path('logout/', LogoutView.as_view(), name='logout'),
-               path('', include('django.contrib.auth.urls'))]
+                path('logout/', LogoutView.as_view(), name='logout'),
+                path('register/', RegisterView.as_view(), name='register'),
+                path('profile/', edit_profile, name='profile'), 
+                path('', include('django.contrib.auth.urls'))]
--- projects_management/settings.py
+++ projects_management/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'debug_toolbar',
+    "debug_toolbar",
     'accounts',
     'projects',
 ]
@@ -52,7 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 
 ]
 
